ebe7ba4af02eea99573f6b88b186a22b335966b3
Pavel Tvaladze <github@ptdev.pw>
2016-08-17 00:06:00 +0200

    testing hooks

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_name("speakup_session");
 require('../default/scripts/engine/config.php');
 require('../default/scripts/engine/functions.php');
 
-$version = "1.8.12.1";
+$version = "1.8.12.2";
 # define("DEBUG", 1);
 
 class CodeGen {
